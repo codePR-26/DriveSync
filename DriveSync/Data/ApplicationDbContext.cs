2ed5e48b76e5dc049649add4d94653ff09ac73f2
Prithwish Bhowmik <prithmid@gmail.com>
2026-02-25 22:32:08 +0530

    "Registered DbContext in Program.cs"

---
+++ DriveSync/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DriveSync.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
