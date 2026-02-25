4532e9e39b94ffa1bb8dcdfc570fe95f65102a4e
Prithwish Bhowmik <prithmid@gmail.com>
2026-02-25 22:53:54 +0530

    "Created Vehicle model"

---
+++ DriveSync/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriveSync.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        [Required]
+        public decimal PricePerDay { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+    }
+}
